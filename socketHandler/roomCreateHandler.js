24d6e863f1309b4684a0ad9358f0528675a9d02d
anshu7sah <anshu7sah@gmail.com>
2023-12-26 11:21:23 +0545

    tested a function

--- socketHandler/roomCreateHandler.js
+++ socketHandler/roomCreateHandler.js
@@ -4,6 +4,7 @@ const { updateRooms } = require("./updates/rooms");
 const roomCreateHandler = (socket) => {
   const socketId = socket.id;
   const userId = socket.user.userId;
+  console.log("in socketHandler in room create Handler");
 
   const roomDetails = addNewActiveRoom(userId, socketId);
 
